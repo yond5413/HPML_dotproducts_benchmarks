0463440d19c2c965c7cacbdf60ff3718ccfc84b6
yond5413 <yd2696@columbia.edu>
2024-02-05 02:57:04 -0500

    updates to dp2, dp3

--- dp2.c
+++ dp2.c
@@ -66,7 +66,7 @@ int main( int argc, char *argv[] )
     */
     exec_time /= half;
     exec_time *=0.000001; //for microseconds 
-    float b = 8.0*(100000000)/exec_time;//sizeof(float)*(measurements*vec_size);
+    float b = 8.0*(measurements*vec_size)/exec_time;//sizeof(float)*(measurements*vec_size);
     float F = 2.0/exec_time;
     printf("N: %d, <T>: %f sec, B: %f GB/s, F: %f FLOP/sec \n", vec_size,exec_time,b,F);
     return 0;
